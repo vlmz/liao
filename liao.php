65fa6dead64d66b168fcfa16ef2a4a91c1118459
liaomingzhang <liaomingzhang@baidu.com>
2017-11-14 19:45:23 +0800

    test.

---
+++ liao.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 这是一个测试项目
+ */
+
+namespace Vlmz;
+
+/**
+ * 测试类
+ * Class liao
+ * @package Vlmz
+ */
+class liao
+{
+    public function test($str = '')
+    {
+        return 'is a test' . $str;
+    }
+}
